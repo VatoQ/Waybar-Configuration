eca7798810afc42beede8cd110fc92fcb8f9de85
Jonathan Enns <jonathanenns97@web.de>
2025-07-27 13:00:07 +0200

    add utility to change all font sizes on install

--- install.py
+++ install.py
@@ -1,8 +1,25 @@
 #!/bin/python
+# Copyright (c) 2025 VatoQ
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+
 
 from install_resources import generate_css, generate_json, generate_shell_scripts
 import getpass
 import subprocess
+import argparse
 
 TEST = False
 EXISTS_STATUS = 0
@@ -10,6 +27,16 @@ DOES_NOT_EXIST_STATUS = 1
 
 
 states_found = False
+
+font_size_help = "specify the font size that the waybar elements are displayed at"
+
+arg_parser = argparse.ArgumentParser()
+arg_parser.add_argument(
+    "--font_size", nargs="?", help=font_size_help, const=30, type=int
+)
+args = arg_parser.parse_args()
+
+font_size = args.font_size
 
 
 def get_target_path(test, current_user):
@@ -67,7 +94,7 @@ shell_output = subprocess.run(
 ).stdout.decode()
 
 
-css_content = generate_css(current_user)
+css_content = generate_css(current_user, font_size=font_size)
 
 json_content = generate_json(current_user)
 
